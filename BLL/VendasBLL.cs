77db951ce3eeea6e4afb2e86d789b06da2f39b86
Vinicius Kock <vinikk03@gmail.com>
2019-06-07 15:22:32 -0300

    Adição de Vendas

---
+++ BLL/VendasBLL.cs
@@ -0,0 +1,39 @@
+﻿using DAL;
+using Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class VendasBLL
+    {
+        VendasDAL dal = new VendasDAL();
+        List<string> erros = new List<string>();
+
+        public string Cadastrar(Vendas ven)
+        {
+            if (ven.Quantidade < 1)
+            {
+                erros.Add("Quantidade Requisitada Inválida.");
+            }
+            if (ven.DataDeVenda != DateTime.Now)
+            {
+                erros.Add("Você está a frente do seu tempo.");
+            }
+            if (erros.Count != 0)
+            {
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < erros.Count; i++)
+                {
+                    //Environment.NewLine
+                    builder.AppendLine(erros[i]);
+                }
+                return builder.ToString();
+            }
+            return dal.Inserir(ven);
+        }
+    }
+}
---
+++ DAL/VendasDAL.cs
@@ -0,0 +1,37 @@
+﻿using Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class VendasDAL : IEntityCRUD<Vendas>
+    {
+        public string Atualizar(Vendas item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Excluir(Vendas item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Inserir(Vendas item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vendas LerPorID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Vendas> LerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
